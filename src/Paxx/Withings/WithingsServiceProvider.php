2d8d4fab0305138a1b481edfea4eccc485846535
Alexander <alexander@paxxmedia.se>
2013-09-15 20:45:42 +0200

    Fixed indentation on copy pasted code

--- src/Paxx/Withings/WithingsServiceProvider.php
+++ src/Paxx/Withings/WithingsServiceProvider.php
@@ -6,42 +6,38 @@ use Illuminate\Foundation\AliasLoader;
 class WithingsServiceProvider extends ServiceProvider {
 
 	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
+	* Indicates if loading of the provider is deferred.
+	*
+	* @var bool
+	*/
 	protected $defer = false;
 
-    /**
-     * Bootstrap the application events.
-     *
-     * @return void
-     */
-    public function boot()
-    {
+	/**
+	* Bootstrap the application events.
+	*
+	* @return void
+	*/
+	public function boot()
+	{
+		$this->package('paxx/withings');
 
-        $this->package('paxx/withings');
+		$loader = AliasLoader::getInstance();
 
- 		$loader = AliasLoader::getInstance();
-        
-        $loader->alias('WithingsApi', 'Paxx\Withings\Api');
-        $loader->alias('WithingsAuth', 'Paxx\Withings\Provider\Withings');
+		$loader->alias('WithingsApi', 'Paxx\Withings\Api');
+		$loader->alias('WithingsAuth', 'Paxx\Withings\Provider\Withings');
 
-    }
+	}
 
 	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
+	* Register the service provider.
+	*
+	* @return void
+	*/
 	public function register()
 	{
-
 		$this->app['withingsapi'] = $this->app->share(function($app)
 		{
 			return new Api;
 		});
 	}
-
-
 }
